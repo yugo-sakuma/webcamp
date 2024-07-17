32c1012ca5dd82455e2de34c2403c96ab7d3f976
yugo-sakuma <130280988+yugo-sakuma@users.noreply.github.com>
2024-07-17 13:26:22 +0000

    booker2を完成させた

--- rails6-bookers2-debug-ruby3-main/app/controllers/books_controller.rb
+++ rails6-bookers2-debug-ruby3-main/app/controllers/books_controller.rb
@@ -1,10 +1,11 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
-  
+  before_action :correct_user, only: [:edit, :update, :destroy]
+
   def show
     @book = Book.find(params[:id])
     @new_book = Book.new
-    @user = current_user
+    @user = @book.user
   end
 
   def index
@@ -18,7 +19,7 @@ class BooksController < ApplicationController
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
-      flash.now[:error] = @book.errors.full_messages.join '/'
+      flash.now[:error] = @book.errors.full_messages.join('/')
       @books = Book.all
       render 'index'
     end
@@ -33,14 +34,14 @@ class BooksController < ApplicationController
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
-      flash.now[:error] = @book.errors.full_messages.join '/'
+      flash.now[:error] = @book.errors.full_messages.join('/')
       render "edit"
     end
   end
 
-  def destroy  # deleteからdestroyに変更
+  def destroy
     @book = Book.find(params[:id])
-    @book.destroy  # スペルミスを修正
+    @book.destroy
     redirect_to books_path, notice: "Book was successfully deleted."
   end
 
@@ -48,5 +49,12 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path, alert: "You are not authorized to edit this book."
+    end
   end
 end
--- rails6-bookers2-debug-ruby3-main/app/controllers/users_controller.rb
+++ rails6-bookers2-debug-ruby3-main/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :ensure_correct_user, only: [:update, :show, :edit]
+  before_action :ensure_correct_user, only: [:update, :edit]
 
   def show
     @user = User.find_by(id: params[:id])
