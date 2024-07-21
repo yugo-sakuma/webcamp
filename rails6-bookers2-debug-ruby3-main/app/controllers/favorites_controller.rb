214452ebaa52e779d113eb4ed6b98988a04736cf
yugo-sakuma <130280988+yugo-sakuma@users.noreply.github.com>
2024-07-21 16:26:06 +0000

    非同期処理に変更

--- rails6-bookers2-debug-ruby3-main/app/controllers/favorites_controller.rb
+++ rails6-bookers2-debug-ruby3-main/app/controllers/favorites_controller.rb
@@ -1,17 +1,19 @@
 class FavoritesController < ApplicationController
-  before_action :authenticate_user!
-
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
-    favorite.save
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    current_user.favorites.create(book: @book)
+    respond_to do |format|
+      format.html { redirect_to @book }
+      format.js
+    end
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
-    favorite.destroy
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    current_user.favorites.find_by(book: @book).destroy
+    respond_to do |format|
+      format.html { redirect_to @book }
+      format.js
+    end
   end
 end
